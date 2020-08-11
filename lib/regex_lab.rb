5e1eba71b05f76c65f6b62b9dfda699739c58820
Bria White <bria.awhite@gmail.com>
2020-08-11 17:51:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,6 @@
 def starts_with_a_vowel?(word)
  word.scan(/^[aeiou]/) == true 
-  return true
- else 
-  return false 
- end 
+  return true 
 end 
 
 def words_starting_with_un_and_ending_with_ing(text)
